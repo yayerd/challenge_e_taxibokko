522fd1cca83bcbc83edad3c0b421fffbb34f07df
Yaye Rokhaya <ladyrokhaya@gmail.com>
2023-10-19 15:52:05 +0000

    La page bienvenue

---
+++ welcome.php
@@ -0,0 +1,29 @@
+
+<?php
+// session_start(); // Démarrez la session
+
+// if (isset($_SESSION['user'])) {
+//     $user = $_SESSION['user'];
+//     $prenom = $user['Prenom'];
+//     echo "Bienvenue, $prenom_signup ! Vous êtes connecté.";
+// }// else {
+// //     echo "Identifiants incorrects. Veuillez réessayer.";
+// // }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Page </title>
+</head>
+<body>
+    
+<h1>Welcome to E-Taxi Bokko</h1>
+
+
+</body>
+</html>
